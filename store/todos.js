55b88486b15e704a449a93005428b0399d9ea5c6
yuki <yuki@yuki13.local>
2021-05-13 20:28:51 +0900

    todoの追加、削除、チェック機能追加

---
+++ store/todos.js
@@ -0,0 +1,33 @@
+import firebase from "@/plugins/firebase";
+import { firestoreAction } from "vuexfire";
+
+const db = firebase.firestore();
+const todosRef = db.collection("todos");
+
+export const state = () => ({
+  todos: []
+});
+
+export const action = {
+  init: firestoreAction(({ bindFirestoreRef }) => {
+    bindFirestoreRef("todos", todosRef);
+  }),
+  add: firestoreAction((context, name) => {
+    // 空白スペース削除
+    if (name.trim()) {
+      todosRef.add({
+        name: name,
+        done: false,
+        created: firebase.firestore.FieldValue.serverTimestamp() // これでサーバーが生成したタイムスタンプを取得できる
+      });
+    }
+  }),
+  remove: firestoreAction((context, id) => {
+    todosRef.doc(id).delete();
+  }),
+  toggle: firestoreAction((context, todo) => {
+    todosRef.doc(todo.id).update({
+      done: !todo.done
+    });
+  })
+};
